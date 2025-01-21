d80c5e681bf429211d1e854e8aa92bde0f8ecf2a
Carlos Saldaña <jcarlossaldae@gmail.com>
2025-01-21 16:52:36 +0000

    day 2

--- src/quiz.js
+++ src/quiz.js
@@ -34,4 +34,16 @@ class Quiz {
             return true
         }
     }
+
+    filterQuestionsByDifficulty(difficulty) {
+        if (difficulty < 1 || difficulty > 3 || typeof difficulty !== "number") return
+        const filteredQuestions = this.questions.filter(question => question.difficulty === difficulty)
+        this.questions = filteredQuestions
+    }
+
+    averageDifficulty() {
+        const sumOfDifficulties = this.questions.reduce((acc, question) => acc + question.difficulty, 0)
+        return sumOfDifficulties / this.questions.length
+    }
+
 }
